a2a3ebfdd1c79090ce420af650ac3a4860afb523
Anton Chernysh <external.Anton.Chernysh@de.bosch.com>
2023-05-17 21:46:56 +0200

    Add everything :)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const {invoke} = window.__TAURI__.tauri;
 
 let greetInputEl;
 let greetMsgEl;
+let hubStatus = true;
 
 async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -22,19 +23,47 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const first = document.querySelector("#term-one");
     first.addEventListener("click", selectImage);
+
+    document.querySelector("#discover-terminals")
+            .addEventListener("click", discover);
+
+    document.querySelector("#serial-port-menu")
+            .addEventListener("change", serialPortSelectHandler);
+
 });
 
 console.log("test");
 
-const openModal = () => {
+async function openModal() {
     const modalContainer = document.querySelector("#settings-modal");
+
+    // Serial data obtaining
+    var result = await invoke("discover_serial_ports");
+
+    console.info("openModal: result = " + result);
+
+    // Fill serial port menu
+    var serialPortMenu = document.querySelector("#serial-port-menu");
+
+    result.forEach((portName) => {
+        var optionElement = document.createElement("option");
+
+        optionElement.innerText = portName;
+
+        serialPortMenu.appendChild(optionElement);
+    });
+
+
+    setHubStatus(hubStatus);
+    hubStatus = !hubStatus;
+
 
     modalContainer.style.display = "block";
     modalContainer.offsetHeight;
     modalContainer.style.opacity = 1;
 };
 
-const closeModal = () => {
+function closeModal() {
     const modalContainer = document.querySelector("#settings-modal");
 
     modalContainer.style.opacity = 0;
@@ -66,4 +95,36 @@ function selectImage() {
 
     // Trigger a click event on the file input element
     fileInput.click();
+}
+
+function setHubStatus(isDetected) {
+    var hubStatus = document.querySelector(".hub-status");
+
+    if (isDetected) {
+        hubStatus.textContent = "Detected";
+        hubStatus.className = "hub-status detected";
+    } else {
+        hubStatus.textContent = "Not detected";
+        hubStatus.className = "hub-status not-detected";
+    }
+}
+
+async function discover() {
+    var channelIdObject = document.querySelector("#radio-channel");
+    var result = await invoke("discover_terminals",
+                              {channelId: parseInt(channelIdObject.value)});
+
+    console.info("result = " + result);
+}
+
+async function serialPortSelectHandler(event) {
+    // Get the selected option value
+    var selectedOption = event.target.value;
+
+    // Perform actions based on the selected option
+    console.log("Selected option:", selectedOption);
+
+    var result = await invoke("open_selected_port", {path: selectedOption});
+
+    console.info("serialPortSelectHandler: result = " + result);
 }
