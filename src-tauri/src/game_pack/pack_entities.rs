c49920784a6dfb0cd6b89d92bb9a849c87b3a877
Anton Chernysh <external.Anton.Chernysh@de.bosch.com>
2023-05-21 13:56:58 +0200

    back: parser: Add pack mapper

---
+++ src-tauri/src/game_pack/pack_entities.rs
@@ -0,0 +1,64 @@
+// Game entities
+#[derive(Debug, PartialEq, Clone)]
+pub enum AtomType {
+    Say,
+    Voice,
+    Video,
+    Marker,
+    Image,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Atom {
+    pub atom_type: AtomType,
+    pub content: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Question {
+    pub scenario: Vec<Atom>,
+    pub right_answer: String,
+    pub price: u32,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Theme {
+    pub name: String,
+    pub questions: Vec<Question>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum RoundType {
+    Normal,
+    Final,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Round {
+    pub name: String,
+    pub round_type: RoundType,
+    pub themes: Vec<Theme>,
+}
+
+// Pack information
+#[derive(Debug, PartialEq, Clone)]
+pub struct Author {
+    pub name: String,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Info {
+    pub authors: Vec<Author>,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Package {
+    pub name: String,
+    pub version: String,
+    pub id: String,
+    pub restriction: String,
+    pub date: String,
+    pub difficulty: u8,
+    pub info: Info,
+    pub rounds: Vec<Round>,
+}
--- src-tauri/src/game_pack/pack_loader.rs
+++ src-tauri/src/game_pack/pack_loader.rs
@@ -10,3 +10,85 @@ fn parse_package(file_path: &str) -> PackageDto {
 
     from_str(&package_xml).unwrap()
 }
+
+fn map_package(dto: PackageDto) -> Package {
+    let map_atoms = |a: &AtomDto| Atom {
+        atom_type: {
+            match a.r#type {
+                AtomTypeDto::say => AtomType::Say,
+                AtomTypeDto::voice => AtomType::Voice,
+                AtomTypeDto::video => AtomType::Video,
+                AtomTypeDto::marker => AtomType::Marker,
+                AtomTypeDto::image => AtomType::Image,
+            }
+        },
+        content: a.content.clone(),
+    };
+
+    let map_questions = |q: &QuestionDto| Question {
+        price: q.price,
+        scenario: {
+            q.scenario
+                .atoms_list
+                .iter()
+                .map(map_atoms)
+                .collect::<Vec<Atom>>()
+        },
+        right_answer: q.right.answer.clone(),
+    };
+
+    let map_themes = |t: &ThemeDto| Theme {
+        name: t.name.clone(),
+        questions: {
+            t.questions
+                .questions_list
+                .iter()
+                .map(map_questions)
+                .collect::<Vec<Question>>()
+        },
+    };
+
+    let map_rounds = |r: &RoundDto| Round {
+        name: r.name.clone(),
+        round_type: {
+            match r.r#type {
+                RoundTypeDto::normal => RoundType::Normal,
+                RoundTypeDto::r#final => RoundType::Final,
+            }
+        },
+        themes: {
+            r.themes
+                .themes_list
+                .iter()
+                .map(map_themes)
+                .collect::<Vec<Theme>>()
+        },
+    };
+
+    Package {
+        name: dto.name,
+        version: dto.version,
+        id: dto.id,
+        restriction: dto.restriction,
+        date: dto.date,
+        difficulty: dto.difficulty,
+        info: Info {
+            authors: {
+                dto.info
+                    .authors
+                    .iter()
+                    .map(|a| Author {
+                        name: a.name.clone(),
+                    })
+                    .collect::<Vec<Author>>()
+            },
+        },
+        rounds: {
+            dto.rounds
+                .rounds_list
+                .iter()
+                .map(map_rounds)
+                .collect::<Vec<Round>>()
+        },
+    }
+}
