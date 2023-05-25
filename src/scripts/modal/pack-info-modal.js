f23407992e9b4ece7cb536ac71ec883fa7035a4b
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-05-25 22:58:10 +0200

    front: Add pack info logic processing

--- src/scripts/modal/pack-info-modal.js
+++ src/scripts/modal/pack-info-modal.js
@@ -1,13 +1,56 @@
-import {openModal, closeModal} from "./modal-common.js"
-import {getPackInfo} from "./../back-end-com.js";
-
+import { openModal, closeModal } from "./modal-common.js";
+import { getPackInfo } from "./../back-end-com.js";
 
 export async function openPackInfoModal() {
     const modalPackInfoContainer = document.querySelector("#pack-info-modal");
 
+    const filePath = ""; // selectPackFile();
+    const packInfo = getPackInfo(filePath);
 
-    
+    setPackName(packInfo.packName);
+    setPackAuthor(packInfo.packAuthor);
+    setPackRounds(packInfo.packRounds);
+    setPackTopics(packInfo.packTopics);
+    setPackQuestion(packInfo.packQuestion);
+    setPackTopicList(packInfo.packTopicList);
+
     openModal(modalPackInfoContainer);
+}
+
+function setPackTopicList(packTopicList) {
+    const packTopicListElement = document.querySelector("#topic-list");
+    packTopicListElement.innerHTML = "";
+
+    packTopicList.forEach((topic) => {
+        let li = document.createElement("li");
+        li.innerText = topic;
+        packTopicListElement.appendChild(li);
+    });
+}
+
+function setPackQuestion(packQuestion) {
+    const packQuestionElement = document.querySelector("#pack-question-num");
+    packQuestionElement.innerText = "Question: " + packQuestion;
+}
+
+function setPackTopics(packTopics) {
+    const packTopicsElement = document.querySelector("#pack-topics-num");
+    packTopicsElement.innerText = "Topics: " + packTopics;
+}
+
+function setPackRounds(packRounds) {
+    const packRoundsElement = document.querySelector("#pack-round-num");
+    packRoundsElement.innerText = "Rounds: " + packRounds;
+}
+
+function setPackName(packName) {
+    const packNameElement = document.querySelector("#pack-name");
+    packNameElement.innerText = "Pack: " + packName;
+}
+
+function setPackAuthor(packAuthor) {
+    const packAuthorElement = document.querySelector("#pack-author-form");
+    packAuthorElement.innerText = "Author: " + packAuthor;
 }
 
 export function closePackInfoModal() {
@@ -15,4 +58,3 @@ export function closePackInfoModal() {
 
     closeModal(modalPackInfoContainer);
 }
-
