a5d1861b554895e1196939ccc3bc4f134c52c5fd
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-05-28 23:31:34 +0200

    all: Update player initialization

--- src-tauri/src/api/mapper.rs
+++ src-tauri/src/api/mapper.rs
@@ -1,9 +1,9 @@
-#[allow(unused_imports, unused_variables)]
-
-use std::ops::Deref;
 use crate::api::dto::ConfigDto;
 use crate::core::game_entities::{game_ctx, Player};
 use crate::hw_comm::api::discover_serial_ports;
+use std::collections::HashMap;
+#[allow(unused_imports, unused_variables)]
+use std::ops::Deref;
 
 use super::dto::PlayerSetupDto;
 
@@ -14,16 +14,16 @@ pub fn get_config_dto() -> ConfigDto {
         available_ports: discover_serial_ports(),
         hub_port: context.hub.port.clone(),
         radio_channel: context.hub.radio_channel,
-        players: context.players
+        players: context
+            .players
             .iter()
-            .map(|p| {
-                PlayerSetupDto {
-                    icon: p.icon.clone(),
-                    is_used: p.is_used,
-                    name: p.name.clone(),
-                    term_id: p.term_id
-                }
-            }).collect(),
+            .map(|p| PlayerSetupDto {
+                icon: p.1.icon.clone(),
+                isUsed: p.1.is_used,
+                name: p.1.name.clone(),
+                termId: p.1.term_id,
+            })
+            .collect(),
     }
 }
 
@@ -38,5 +38,12 @@ pub fn get_config_dto() -> ConfigDto {
 /// Takes whole game context and maps to config which contains only required elements
 pub fn update_players(players: &Vec<Player>) {
     let mut context = game_ctx();
-    context.players = players.clone();
+
+    let players_map: HashMap<String, Player> = HashMap::new();
+
+    context.players = players.iter()
+        .fold(HashMap::new(), |mut map, player| {
+        map.insert(player.name.clone(), player.clone());
+        map
+    });
 }
