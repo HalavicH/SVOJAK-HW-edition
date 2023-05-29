8913af4933a19cec67a7986d40dcc0d2858396bb
Anton Chernysh <external.Anton.Chernysh@de.bosch.com>
2023-05-29 19:07:39 +0200

    back: game-process: Add game loader

--- src-tauri/src/game_process/game_processor.rs
+++ src-tauri/src/game_process/game_processor.rs
@@ -3,6 +3,9 @@ use std::sync::Arc;
 use tempfile::TempDir;
 use zip::ZipArchive;
 
+use crate::game_pack::pack_loader::load_pack;
+use crate::game_process::game_info::*;
+
 fn create_temp_directory() -> Arc<TempDir> {
     let temp_dir = Arc::new(TempDir::new().expect("Failed to create temporary directory"));
 
@@ -17,4 +20,26 @@ fn unarchive_zip(archive_path: &str, directory_path: &str) {
     archive
         .extract(directory_path)
         .expect("Failed to unpack archive");
+}
+
+pub fn load_game(game_archive_path: &str) -> GameInstance {
+    let temp_dir = create_temp_directory();
+    let temp_dir_path = temp_dir.path();
+
+    unarchive_zip(game_archive_path, temp_dir_path.to_str().unwrap());
+
+    let game_info = GameInfo {
+        pack_content_dir: temp_dir.clone(),
+        pack_content_file_path: temp_dir_path.join(PACKAGE_CONTENT_FILE_NAME),
+        pack_audio_path: temp_dir_path.join(PACKAGE_AUDIO_DIR_NAME),
+        pack_images_path: temp_dir_path.join(PACKAGE_IMAGES_DIR_NAME),
+        pack_video_path: temp_dir_path.join(PACKAGE_VIDEO_DIR_NAME),
+    };
+
+    let game_package = load_pack(&game_info);
+
+    GameInstance {
+        information: game_info.clone(),
+        package: game_package.clone(),
+    }
 }
