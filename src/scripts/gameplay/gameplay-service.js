6351df7056b4e218be94c5ab28bf69951a477457
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-05-30 21:55:37 +0200

    front: Add impl for question screen buttons

--- src/scripts/gameplay/gameplay-service.js
+++ src/scripts/gameplay/gameplay-service.js
@@ -1,4 +1,4 @@
-import { getActivePlayerId, answerQuestion, getQuestionData } from "../service/back-end-com.js";
+import { getActivePlayerId, answerQuestion, getQuestionData, allowAnswer, waitForFirstClick } from "../service/back-end-com.js";
 import { processPipPlayers } from "./modal/pig-in-poke-modal.js";
 import { processAuctionPlayers } from "./modal/auction-modal.js";
 
@@ -21,10 +21,15 @@ export async function processQuestionSelection(event) {
 export async function processQustionDisplay(topic, price) {
     console.log("Retreiving question '" + topic + ":" + price + "'");
     const question = await getQuestionData(topic, price);
-    console.log("Response"
-        + ". questionType: " + question.questionType
-        + ", mediaType: " + question.mediaType
-        + ", content: " + question.content);
+    console.log(
+        "Response" +
+            ". questionType: " +
+            question.questionType +
+            ", mediaType: " +
+            question.mediaType +
+            ", content: " +
+            question.content
+    );
 
     if (question.questionType === "normal") {
         displayQuestionScreen();
@@ -49,47 +54,73 @@ export function displayRoundScreen() {
     // Disable question viewport
     const questionViewport = document.querySelector("#question-screen");
     questionViewport.style.display = "none";
-    
+
     // Enable round viewport
     const roundViewport = document.querySelector("#round-screen");
     roundViewport.style.display = "flex";
 }
 
-export async function processCorrectAnswer(event) {
+export async function processCorrectAnswer() {
     const player = await answerQuestion(true);
-    updateUserScore(player.id);
-
-    // TODO: Clear player selection (.className = "player-badge")
-    // TODO: set active player badge
-        // 1. getActivePlayerId()
-        // 2. .className = "player-badge topic-selection"
-
-    displayRoundScreen();    
+    updateUserScore(player);
+    goToRoundScreen();
 }
 
-export async function processWrongAnswer(event) {
+export function goToRoundScreen() {
+    setAllPlayersState("");
+    setActivePlayerBadgeState("topic-selection");
+    displayRoundScreen();
+}
+
+export async function processWrongAnswer() {
     console.log("Activeeeee!!!!!!!!!!!!!");
 
     const player = await answerQuestion(false);
     updateUserScore(player.id);
 
     // TODO: set active player badge
-        // 1. getActivePlayerId()
-        // 2. .className = "player-badge inactive"
-        // 3. forbidAnswer()
-        // 4. listen
+    // 1. getActivePlayerId()
+    // 2. .className = "player-badge inactive"
+    // 3. forbidAnswer()
+    // 4. listen
 
     // TODO: make player inactive, set answer forbidden
-
 }
 
-export async function updateUserScore(playerId) {
+export async function allowAnswerHandler() {
+    allowAnswer();
+    await waitForFirstClick();
+    setActivePlayerBadgeState("first-response");
+}
+
+export async function updateUserScore(responcePlayer) {
     const playerBadges = document.querySelector("#player-list").querySelectorAll(".player-badge");
 
     playerBadges.forEach((player) => {
-        if (playerId === player.querySelector(".player-details-id").innerText) {
-            player.querySelector(".player-details-score-value").innerText = response.newScore;
+        if (responcePlayer.id == player.querySelector(".player-details-id").innerText) {
+            player.querySelector(".player-details-score-value").innerText = responcePlayer.newScore;
         }
     });
 }
 
+export function setAllPlayersState(state) {
+    const playerList = document.querySelector("#player-list");
+    const playerBadges = playerList.querySelectorAll(".player-badge");
+
+    playerBadges.forEach((player) => {
+        player.className = "player-badge " + state;
+    });
+}
+
+export async function setActivePlayerBadgeState(state) {
+    const activePlayer = await getActivePlayerId();
+    const playerList = document.querySelector("#player-list");
+    const playerBadges = playerList.querySelectorAll(".player-badge");
+
+    playerBadges.forEach((player) => {
+        const id = player.querySelector(".player-details-id").innerText;
+        if (id == activePlayer) {
+            player.className = "player-badge " + state;
+        }
+    });
+}
---
+++ src/scripts/gameplay/modal/auction-modal.js
@@ -0,0 +1,69 @@
+import { fetchPlayers, sendPipVictim } from "../../service/back-end-com.js";
+import { closeModal, openModal } from "../../service/modal-common.js";
+import { getImagePathOrDefault } from "../../service/utils.js";
+import { displayQuestionScreen } from "../gameplay-service.js";
+
+
+export async function processAuctionPlayers(activePlayerId) {
+    const players = await fetchPlayers();
+    const playerList = document.querySelector("#player-auction-list");
+    playerList.innerHTML = "";
+
+    players.forEach((player) => {
+        let playerBadge = document.createElement("div");
+        playerBadge.className = "player-auction-badge";
+        // playerBadge.addEventListener("click", processVictimSelection);
+        playerBadge.style.cursor = "pointer";
+        playerList.appendChild(playerBadge);
+
+        let playerIcon = document.createElement("div");
+        playerIcon.className = "player-icon";
+        playerBadge.appendChild(playerIcon);
+
+        let icon = document.createElement("img");
+        icon.src = getImagePathOrDefault(player.playerIconPath);
+        playerIcon.appendChild(icon);
+ 
+        let playersDetails = document.createElement("div");
+        playersDetails.className = "player-details";
+        playerBadge.appendChild(playersDetails);
+    
+        let playerDetailsId = document.createElement("div");
+        playerDetailsId.className = "player-details-id";
+        playerDetailsId.style.display = "none";
+        playerDetailsId.innerText = player.id;
+        playersDetails.appendChild(playerDetailsId);
+    
+        let playerDetailsName = document.createElement("p");
+        playerDetailsName.className = "player-details-name";
+        playerDetailsName.innerText = player.playerName;
+        playersDetails.appendChild(playerDetailsName);
+
+        let playerBid = document.createElement("div");
+        playerBid.className = "player-details-bid";
+        playerBid.innerText = "Bid: ";
+        playersDetails.appendChild(playerBid);
+
+        let bid = document.createElement("p");
+        bid.className = "player-details-bid-value";
+        bid.innerText = 0;
+        playerBid.appendChild(bid);
+    
+        let playerDetailsScore = document.createElement("div");
+        playerDetailsScore.className = "player-details-score";
+        playerDetailsScore.innerText = "Score: ";
+        playersDetails.appendChild(playerDetailsScore);
+    
+        let score = document.createElement("p");
+        score.className = "player-details-score-value";
+        score.innerText = player.score;
+        playerDetailsScore.appendChild(score);    
+    });
+
+    const modal = document.querySelector("#auction-modal");
+
+    openModal(modal);
+
+    // TODO: Auction logic
+}
+
