a4fbcec6a4091de44f0302234afd7fba30b5d778
Oleksandr Kholiavko <oleksandr.kholiavko@innovecs.com>
2023-06-02 15:25:02 +0200

    back: Add question fetching API

--- src-tauri/src/core/game_entities.rs
+++ src-tauri/src/core/game_entities.rs
@@ -1,11 +1,10 @@
 #[allow(unused_imports, unused_variables)]
-
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::collections::HashMap;
 
 use serde::{Serialize, Deserialize};
-use crate::game_pack::pack_entities::PackContent;
+use crate::game_pack::pack_entities::{PackContent, Question, Round};
 
 #[derive(Debug, Eq, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Player {
@@ -64,7 +63,39 @@ impl HubManager {
 pub struct GameContext {
     pub players: HashMap<u8, Player>,
     pub pack: PackContent,
+    pub current_round_index: i32,
     pub hub: HubManager,
+}
+
+impl GameContext {
+    pub fn get_current_round(&self) -> &Round {
+        let index = self.current_round_index as usize;
+        let mut round = self.pack.rounds.get(index).unwrap();
+        println!("Current round: #{index} name: {}", round.name);
+        &round
+    }
+
+    pub fn pop_question(&mut self, theme: &String, price: &i32) -> Result<(Question, i32), String> {
+        println!("Get question from category: {theme}, price: {price}");
+        let round = self.get_current_round_mut();
+        let mut theme = round.themes.get_mut(theme).ok_or("Theme not found".to_string())?;
+        let question = theme.pop_question(price).ok_or("Question not found".to_string())?.clone();
+        round.questions_left -= 1;
+        println!("Question left: {}", round.questions_left);
+
+        let question_number = round.question_count - round.questions_left;
+        Ok((question, question_number))
+    }
+
+    pub(crate) fn is_last_question(&self) -> bool {
+        self.get_current_round().questions_left <= 1
+    }
+
+    fn get_current_round_mut(&mut self) -> &mut Round {
+        let index = self.current_round_index as usize;
+        let mut round = self.pack.rounds.get_mut(index).unwrap();
+        round
+    }
 }
 
 lazy_static::lazy_static! {
