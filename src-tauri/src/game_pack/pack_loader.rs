dbff4e0ff14abff36d8d409e1addedc7446e6e78
Oleksandr Kholiavko <oleksandr.kholiavko@innovecs.com>
2023-06-02 15:24:40 +0200

    back: Update pack structure

--- src-tauri/src/game_pack/pack_loader.rs
+++ src-tauri/src/game_pack/pack_loader.rs
@@ -1,11 +1,17 @@
+use std::collections::HashMap;
 use serde_xml_rs::from_str;
 use std::fs;
 
 use crate::game_pack::pack_dto::*;
 use crate::game_pack::pack_entities::*;
+use crate::game_process::game_info::{GamePack, PackLocationData};
 
-use crate::game_process::game_info::PackLocationData;
+pub fn load_pack(game_information: &PackLocationData) -> PackContent {
+    let package_content_file_str = game_information.content_file_path.to_str().unwrap();
+    let package: PackageDto = parse_package(package_content_file_str);
 
+    map_package(package)
+}
 fn parse_package(file_path: &str) -> PackageDto {
     let package_xml =
         fs::read_to_string(file_path).expect("Should have been able to read the file");
@@ -14,59 +20,6 @@ fn parse_package(file_path: &str) -> PackageDto {
 }
 
 fn map_package(dto: PackageDto) -> PackContent {
-    let map_atoms = |a: &AtomDto| Atom {
-        atom_type: {
-            match a.r#type {
-                AtomTypeDto::say => AtomType::Say,
-                AtomTypeDto::voice => AtomType::Voice,
-                AtomTypeDto::video => AtomType::Video,
-                AtomTypeDto::marker => AtomType::Marker,
-                AtomTypeDto::image => AtomType::Image,
-            }
-        },
-        content: a.content.clone(),
-    };
-
-    let map_questions = |q: &QuestionDto| Question {
-        price: q.price,
-        scenario: {
-            q.scenario
-                .atoms_list
-                .iter()
-                .map(map_atoms)
-                .collect::<Vec<Atom>>()
-        },
-        right_answer: q.right.answer.clone(),
-    };
-
-    let map_themes = |t: &ThemeDto| Theme {
-        name: t.name.clone(),
-        questions: {
-            t.questions
-                .questions_list
-                .iter()
-                .map(map_questions)
-                .collect::<Vec<Question>>()
-        },
-    };
-
-    let map_rounds = |r: &RoundDto| Round {
-        name: r.name.clone(),
-        round_type: {
-            match r.r#type {
-                RoundTypeDto::normal => RoundType::Normal,
-                RoundTypeDto::r#final => RoundType::Final,
-            }
-        },
-        themes: {
-            r.themes
-                .themes_list
-                .iter()
-                .map(map_themes)
-                .collect::<Vec<Theme>>()
-        },
-    };
-
     PackContent {
         name: dto.name,
         version: dto.version,
@@ -89,15 +42,81 @@ fn map_package(dto: PackageDto) -> PackContent {
             dto.rounds
                 .rounds_list
                 .iter()
-                .map(map_rounds)
+                .map(map_round)
                 .collect::<Vec<Round>>()
         },
     }
 }
 
-pub fn load_pack(pack_location: &PackLocationData) -> PackContent {
-    let package_content_file_str = pack_location.content_file_path.to_str().unwrap();
-    let package: PackageDto = parse_package(package_content_file_str);
+fn map_atoms(a: &AtomDto) -> Atom {
+    Atom {
+        atom_type: {
+            match a.r#type {
+                AtomTypeDto::say => QuestionMediaType::Say,
+                AtomTypeDto::voice => QuestionMediaType::Voice,
+                AtomTypeDto::video => QuestionMediaType::Video,
+                AtomTypeDto::marker => QuestionMediaType::Marker,
+                AtomTypeDto::image => QuestionMediaType::Image,
+            }
+        },
+        content: a.content.clone(),
+    }
+}
 
-    map_package(package)
+fn map_question(q: &QuestionDto) -> Question {
+    Question {
+        price: q.price,
+        scenario: {
+            q.scenario
+                .atoms_list
+                .iter()
+                .map(map_atoms)
+                .collect::<Vec<Atom>>()
+        },
+        right_answer: q.right.answer.clone(),
+    }
+}
+
+fn map_theme(t: &ThemeDto) -> (String, Theme) {
+    (
+        t.name.clone(),
+        Theme {
+            name: t.name.clone(),
+            questions: {
+                t.questions
+                    .questions_list
+                    .iter()
+                    .map(|q| (q.price, { map_question(q) }))
+                    .collect::<HashMap<i32, Question>>()
+            },
+        }
+    )
+}
+
+fn map_round(r: &RoundDto) -> Round {
+    let mut round = Round {
+        name: r.name.clone(),
+        round_type: {
+            match r.r#type {
+                RoundTypeDto::normal => RoundType::Normal,
+                RoundTypeDto::r#final => RoundType::Final,
+            }
+        },
+        themes: {
+            r.themes
+                .themes_list
+                .iter()
+                .map(map_theme)
+                .collect::<HashMap<String, Theme>>()
+        },
+        questions_left: -1,
+        question_count: -1,
+    };
+    let vec = Vec::from_iter(round.themes.values());
+    round.question_count = vec.iter()
+        .map(|&theme| { theme.questions.len() as i32 })
+        .sum::<i32>();
+
+    round.questions_left = round.question_count;
+    round
 }
