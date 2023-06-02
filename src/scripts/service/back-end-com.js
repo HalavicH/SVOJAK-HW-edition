8e5ee1fba8a699e170a40ff2fcbdaf35a53fbde2
HalavicH <halavich@gmail.com>
2023-06-02 15:23:43 +0200

    front: Update gameplay logic and styles
    - Update API
    - Update positioning of question metadata

--- src/scripts/service/back-end-com.js
+++ src/scripts/service/back-end-com.js
@@ -1,178 +1,59 @@
-const { invoke } = window.__TAURI__.tauri;
+const {invoke} = window.__TAURI__.tauri;
 
 export async function getSettingsConfig() {
     return await invoke("fetch_configuration");
 }
 
 export async function discoverHub(selectedOption) {
-    const result = await invoke("discover_hub", { path: selectedOption });
+    const result = await invoke("discover_hub", {path: selectedOption});
+}
+
+export async function discoverTerminals(channelIdObject) {
+    return await invoke("discover_terminals", {
+        channelId: parseInt(channelIdObject.value),
+    });
 }
 
 export async function savePlayers(playersList) {
     playersList.forEach((player) => {
         console.log("Saved player" +
-        "id: " + player.terminalId +
-        "iconPath: " + player.playerIconPath +
-        "name: " + player.playerName + 
-        "used: " + player.used);
+            "id: " + player.terminalId +
+            "iconPath: " + player.playerIconPath +
+            "name: " + player.playerName +
+            "used: " + player.used);
     });
 
-    await invoke("save_players", { players: playersList });
+    await invoke("save_players", {players: playersList});
 }
 
 export async function saveRoundDuration(roundDurationMinutes) {
     console.log("Round duration: " + roundDurationMinutes);
-    const result = await invoke("save_round_duration", { durationMin: roundDurationMinutes });
+    invoke("save_round_duration", {durationMin: roundDurationMinutes});
 }
 
 export async function getPackInfo(pathToPack) {
     return await invoke("get_pack_info");
-    return {
-        packName: "Скрябін",
-        packAuthor: "Кузьма",
-        packRounds: 1,
-        packTopics: 10,
-        packQuestion: 16,
-        packTopicList: ["фsss", "фooo", "фmmm", "фbbb", "rrr", "aaa", "Fallout Equestria"],
-    };
 }
 
 export async function fetchPlayers() {
-    return [
-        {
-            id: 1,
-            playerIconPath: "./assets/default-icon.png",
-            playerName: "Button",
-            score: 200,
-        },
-        {
-            id: 2,
-            playerIconPath: "",
-            playerName: "Button2",
-            score: 500,
-        },
-        {
-            id: 3,
-            playerIconPath: "",
-            playerName: "Button3",
-            score: 200,
-        },
-        {
-            id: 4,
-            playerIconPath: "",
-            playerName: "Button4",
-            score: 700,
-        },
-        {
-            id: 5,
-            playerIconPath: "",
-            playerName: "Button5",
-            score: -100,
-        },
-        {
-            id: 6,
-            playerIconPath: "",
-            playerName: "Button2",
-            score: 500,
-        },
-        {
-            id: 7,
-            playerIconPath: "",
-            playerName: "Button3",
-            score: 200,
-        },
-        {
-            id: 8,
-            playerIconPath: "",
-            playerName: "Button4",
-            score: 700,
-        },
-        {
-            id: 9,
-            playerIconPath: "",
-            playerName: "Жерти",
-            score: -100,
-        },
-    ];
+    return await invoke("fetch_players");
 }
 
-export function fetchRound() {
-    return {
-        roundName: "1",
-        roundTopics: [
-            {
-                topicName: "Festivals",
-                questions: {
-                    price1: 1000,
-                    price2: 2000,
-                    price3: 3000,
-                    price4: 4000,
-                    price5: 5000,
-                },
-            },
-            {
-                topicName: "Meme",
-                questions: {
-                    price1: 1000,
-                    price2: 2000,
-                    price3: 3000,
-                    price4: 4000,
-                    price5: 5000,
-                },
-            },
-            {
-                topicName: "Amogus",
-                questions: {
-                    price1: 1000,
-                    price2: 2000,
-                    price3: 3000,
-                    price4: 4000,
-                    price5: 5000,
-                },
-            },
-            {
-                topicName: "G4",
-                questions: {
-                    price1: 1000,
-                    price2: 2000,
-                    price3: 3000,
-                    price4: 4000,
-                    price5: 5000,
-                },
-            },
-            {
-                topicName: "Music",
-                questions: {
-                    price1: 1000,
-                    price2: 2000,
-                    price3: 3000,
-                    price4: 4000,
-                    price5: 5000,
-                },
-            },
-            {
-                topicName: "Fanfics",
-                questions: {
-                    price1: 1000,
-                    price2: 2000,
-                    price3: 3000,
-                    price4: 4000,
-                    price5: 5000,
-                },
-            },
-        ],
-    };
+export async function fetchRound() {
+    return await invoke("fetch_round");
 }
 
 export async function getQuestionData(topic, price) {
-    return {
-        questionType: "pig-in-poke", // "normal", "pig-in-poke", "auction"
-        mediaType: "text", // "text", "video", "music", "picture"
-        content: "What is the best pone?",
-    };
+    return await invoke("get_question_data", {topic: topic, price: price});
+}
+
+export async function hasNextQuestion() {
+    return await invoke("has_next_question");
 }
 
 export async function getFastestClick() {
+    return await invoke("get_fastest_click");
+
     return {
         newUpdatesPresent: true,
         userWithFastestClick: "Button",
@@ -180,6 +61,8 @@ export async function getFastestClick() {
 }
 
 export async function answerQuestion(answeredCorrectly) {
+    return await invoke("answer_question", {answeredCorrectly: answeredCorrectly});
+
     return {
         // TODO: Add id and check by id
         id: 1,
@@ -187,12 +70,13 @@ export async function answerQuestion(answeredCorrectly) {
     };
 }
 
-export async function sendPipVictim(victimName) {
-    console.log(victimName);
+export async function sendPipVictim(victimId) {
+    console.log(victimId);
+    return await invoke("send_pip_victim", {victimId: victimId});
 }
 
 export async function getActivePlayerId() {
-    return 1;
+    return await invoke("get_active_player_id");
 }
 
 export async function allowAnswer() {
