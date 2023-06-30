de5f5675e55d135b7e37369f6c44fce67cf26f98
HalavicH <halavich@gmail.com>
2023-06-30 15:13:56 +0200

    back: Refactor

--- src-tauri/src/hub_comm/web/web_hub/internal_api.rs
+++ src-tauri/src/hub_comm/web/web_hub/internal_api.rs
@@ -1,21 +1,13 @@
 #![allow(unused)]
 
-use rocket::{Build, Rocket, routes, get, post};
-use rocket::serde::json::{Json, Value};
-use rocket::State;
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use rgb::{RGB, RGB8};
-use rocket::form::FromForm;
-use rocket::fs::{FileServer, relative};
-use rocket::futures::SinkExt;
-use rocket::serde::{Deserialize, Serialize};
+use rocket::{routes, get};
+use rocket::serde::json::{Value};
 use rocket::serde::json::serde_json::json;
 use crate::hub_comm::web::web_hub::server::Persistence;
 
 #[get("/events", format = "application/json")]
 fn get_events(state: Persistence) -> Value {
-    let mut guard = state.lock().expect("Poisoned");
+    let guard = state.lock().expect("Poisoned");
     let events = guard.events.clone();
     log::info!("Events are: {:?}", events);
     json!({"events": events})
