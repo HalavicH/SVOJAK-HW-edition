395ccc928db51009de2f0d8f50a7b64f7a1b0f48
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-07-24 19:47:07 +0200

    front: auction modal refs

--- src/scripts/gameplay/modal/auction-modal.js
+++ src/scripts/gameplay/modal/auction-modal.js
@@ -2,18 +2,21 @@ import { fetchPlayers, sendPipVictim } from "../../service/back-end-com.js";
 import { closeModal, openModal } from "../../service/modal-common.js";
 import { getImagePathOrDefault } from "../../service/utils.js";
 
+const REFS = {
+    playerListDiv: document.querySelector("#player-auction-list"),
+    auctionModal: document.querySelector("#auction-modal"),
+};
 
 export async function processAuctionPlayers(activePlayerId) {
     const players = await fetchPlayers();
-    const playerList = document.querySelector("#player-auction-list");
-    playerList.innerHTML = "";
+    REFS.playerListDiv.innerHTML = "";
 
     players.forEach((player) => {
         let playerBadge = document.createElement("div");
         playerBadge.className = "player-auction-badge";
         // playerBadge.addEventListener("click", processVictimSelection);
         playerBadge.style.cursor = "pointer";
-        playerList.appendChild(playerBadge);
+        REFS.playerListDiv.appendChild(playerBadge);
 
         let playerIcon = document.createElement("div");
         playerIcon.className = "player-icon";
@@ -22,17 +25,17 @@ export async function processAuctionPlayers(activePlayerId) {
         let icon = document.createElement("img");
         icon.src = getImagePathOrDefault(player.playerIconPath);
         playerIcon.appendChild(icon);
- 
+
         let playersDetails = document.createElement("div");
         playersDetails.className = "player-details";
         playerBadge.appendChild(playersDetails);
-    
+
         let playerDetailsId = document.createElement("div");
         playerDetailsId.className = "player-details-id";
         playerDetailsId.style.display = "none";
         playerDetailsId.innerText = player.id;
         playersDetails.appendChild(playerDetailsId);
-    
+
         let playerDetailsName = document.createElement("p");
         playerDetailsName.className = "player-details-name";
         playerDetailsName.innerText = player.playerName;
@@ -47,22 +50,19 @@ export async function processAuctionPlayers(activePlayerId) {
         bid.className = "player-details-bid-value";
         bid.innerText = 0;
         playerBid.appendChild(bid);
-    
+
         let playerDetailsScore = document.createElement("div");
         playerDetailsScore.className = "player-details-score";
         playerDetailsScore.innerText = "Score: ";
         playersDetails.appendChild(playerDetailsScore);
-    
+
         let score = document.createElement("p");
         score.className = "player-details-score-value";
         score.innerText = player.score;
-        playerDetailsScore.appendChild(score);    
+        playerDetailsScore.appendChild(score);
     });
 
-    const modal = document.querySelector("#auction-modal");
-
-    openModal(modal);
+    openModal(REFS.auctionModal);
 
     // TODO: Auction logic
 }
-
